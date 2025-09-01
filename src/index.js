feaaff7b1f95ed4ab67b2790238a5e6cd9682186
breiadeeann <breiaalderson@gmail.com>
2025-09-01 14:23:14 +0200

    Added forecast function to format forecast days

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "3063b23035d47odf96b70c05f0bta423";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -71,17 +78,17 @@ function displayForecast(response) {
         forecastHtml +
         `
 <div class="weather-forecast-day">
-            <div class="weather-forecast-date">Tue</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
             
             <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
           
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>${Math.round(day.temperature.maximum)}</strong>
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
               </div>
               <div class="weather-forecast-temperature">${Math.round(
                 day.temperature.minimum
-              )}</div>
+              )}°</div>
             </div>
           </div>`;
     }
